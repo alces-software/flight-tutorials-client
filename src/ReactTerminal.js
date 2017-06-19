6f84cad0b085094e197c842e79bcfc84fadade59
Ben Armston <ben.armston@alces-software.com>
2017-06-20 00:25:47 +0100

    Improvements to ReactTerminal
     - Disconnect socket.io stream when component unmounts.
     - Size of terminal taken as props.

--- src/ReactTerminal.js
+++ src/ReactTerminal.js
@@ -31,28 +31,36 @@ const socket = io('http://localhost:3001/pty', {path: "/tutorial/socket.io"});
 export default class ReactTerminal extends Component {
   _terminalEl: HTMLPreElement;
   _term: Terminal;
+  _stream: any;
 
   props: {
+    columns: number,
     onInputLine: (string) => void,
+    rows: number,
   }
+
+  static defaultProps = {
+    columns: 80,
+    rows: 50,
+  };
 
   componentDidMount() {
     this.connectTerminal();
   }
 
   componentWillUnmount() {
-    // XXX
-    // this.disconnectTerminal();
+    this.disconnectTerminal();
   }
 
   connectTerminal() {
     debug('Connecting terminal');
     this._terminalEl.tabIndex = 0;
     this._term = new Terminal(this._terminalEl.dataset);
-    const stream = ss.createStream({decodeStrings: false, encoding: 'utf-8'});
-    ss(socket).emit('new', stream, this._terminalEl.dataset);
+    this._stream = ss.createStream({decodeStrings: false, encoding: 'utf-8'});
+    const options = { columns: this.props.columns, rows: this.props.rows };
+    ss(socket).emit('new', this._stream, options);
 
-    stream.on("data", (chunk, ev) => {
+    this._stream.on("data", (chunk, ev) => {
       debug('Received chunk %o as string: %s', chunk, chunk.toString());
       if (chunk[0] === 13 && chunk[1] === 10) {
         debug('Chunk starts with "\\r\\n"');
@@ -61,7 +69,11 @@ export default class ReactTerminal extends Component {
       }
     })
 
-    stream.pipe(this._term).dom(this._terminalEl).pipe(stream);
+    this._stream.pipe(this._term).dom(this._terminalEl).pipe(this._stream);
+  }
+
+  disconnectTerminal() {
+    this._stream.end();
   }
 
   handleInputLine() {
@@ -78,8 +90,8 @@ export default class ReactTerminal extends Component {
         <pre
           ref={(el) => { this._terminalEl = el; } }
           style={terminalStyle}
-          data-columns="80"
-          data-rows="50"
+          data-columns={this.props.columns}
+          data-rows={this.props.rows}
         />
       </div>
     );

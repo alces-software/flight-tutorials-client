4a0ec4535a073bddb7defc3aba3689c5f93c5636
Ben Armston <ben.armston@alces-software.com>
2017-09-12 12:57:47 +0100

    Remove styles

--- src/StarRule.js
+++ src/StarRule.js
@@ -7,15 +7,37 @@
  * All rights reserved, see LICENSE.txt.
  *===========================================================================*/
 import React from 'react';
-
-// import './styles/StarPrimary.scss';
+import styled, { css } from 'styled-components';
 
 type PropTypes = {
   variant: 'primary' | 'light',
 };
 
-const StarRule = ({ variant }: PropTypes) => (
-  <hr className={`star-${variant}`} />
-);
+const StarRule = styled.hr`
+  padding: 0;
+  border: none;
+  border-top: solid 5px;
+  text-align: center;
+  max-width: 250px;
+  margin: 25px auto 30px;
+
+  ${props => props.variant === 'primary' && css`
+    border-color: #2C3E50;
+    &:after {
+      background-color: #fff;
+      color: #2C3E50;
+    }
+  `}
+
+  &:after {
+    content: "\f005";
+    font-family: FontAwesome;
+    display: inline-block;
+    position: relative;
+    top: -.8em;
+    font-size: 2em;
+    padding: 0 .25em;
+  }
+`;
 
 export default StarRule;
